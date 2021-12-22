66f4adc96bcf24186971012bd3360496900f3b3c
Dmitriy <sobolev.dmitriys@yandex.ru>
2021-12-22 21:30:33 +0300

    add migrations

---
+++ src/main/resources/db/migration/v1_0_create_card.sql
@@ -0,0 +1,6 @@
+CREATE TABLE card
+(
+    id            INT     NOT NULL PRIMARY KEY,
+    license_plate VARCHAR NOT NULL,
+    color         VARCHAR NOT NULL
+);
---
+++ src/main/resources/db/migration/v2_0_create_car.sql
@@ -0,0 +1,6 @@
+CREATE TABLE car
+(
+    id            INT     NOT NULL PRIMARY KEY,
+    license_plate VARCHAR NOT NULL,
+    color         VARCHAR NOT NULL
+);
